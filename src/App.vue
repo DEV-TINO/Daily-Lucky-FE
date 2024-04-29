7a518bb151dae58030d8008c447409980d76c0a9
kim-song-jun <>
2024-04-30 00:23:53 +0900

    feat : create Splash Page for test

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,46 @@
 <template>
-  <h4>Hello World</h4>
+  <div class="screen">
+    <SplashPage v-if="currentPage == 1" />
+  </div>
 </template>
 
 <script>
+import SplashPage from "./pages/SplashPage.vue";
+
 export default {
+  components: {
+    SplashPage,
+  },
   data() {
-    return {};
+    return {
+      currentPage: 1, // Splash Page
+    };
   },
 };
 </script>
 
-<style></style>
+<style>
+@font-face {
+  font-family: "custom-font";
+  src: url("@/assets/fonts/custom-font.ttf");
+}
+body {
+  margin: 0;
+  padding: 0;
+  color: black;
+  font-family: "custom-font";
+  background-color: #f8f6e9;
+}
+.screen {
+  width: 393px;
+  height: 852px;
+}
+
+.main-color {
+  color: #958565;
+}
+
+.sub-color {
+  color: #787265;
+}
+</style>
---
+++ src/pages/SplashPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="splash-container">
+    <div class="main-color splash-title">Daily Lucky</div>
+    <img class="splash-image" src="/images/lucky-lucky.png" />
+    <div class="sub-color splash-subtitle">Touch to Start</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SplashPage",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.splash-container {
+  height: 100vh; /* 내가 현재 보이는 화면 크기 전체 높이 */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.splash-image {
+  width: 292px;
+  height: 257px;
+}
+
+.splash-title {
+  margin-top: 100px;
+  font-size: 60px;
+}
+
+.splash-subtitle {
+  font-size: 20px;
+  margin-top: 178px;
+}
+</style>
