ce3a96dd3ff682781698646f6a2781adcc2e620d
GitSojin <dfsa356@naver.com>
2024-05-01 00:23:22 +0900

    feat : create CalendarPage Logo

--- src/pages/CalendarPage.vue
+++ src/pages/CalendarPage.vue
@@ -1,9 +1,42 @@
 <template>
-  <div>캘린더페이지입니다.</div>
+  <div class="calendar-container">
+    <div class="calendar-logo">
+      <img class="calendar-logo-image" src="/public/images/lucky-lucky.png"></img>
+			<div class="calendar-logo-title">Daily Lucky</div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+	name: "CalendarPage",
+	data(){
+		return{}
+	}
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.calendar-container{
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.calendar-logo{
+	margin-top: 51px;
+	height: 68px;
+	width: 366px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.calendar-logo-image{
+	width: 55px;
+	height: 54px;
+}
+.calendar-logo-title{
+	color: #958565;
+}
+
+</style>
