7a79e2adc29af4b641dafcbbc8d2950736231a79
GitSojin <dfsa356@naver.com>
2024-05-01 15:57:12 +0900

    feat : create calendar-day

--- src/pages/CalendarPage.vue
+++ src/pages/CalendarPage.vue
@@ -5,12 +5,23 @@
 			<div class="calendar-logo-title">Daily Lucky</div>
     </div>
 		<div class="calendar-month">
-			<div class="month-leftright calendarMonth"><</div>
-			<div class="month-now calendarMonth">
+			<div class="month-leftright calendar-months"><</div>
+			<div class="month-now calendar-months">
 				<div class="month-now-month">April</div>
 				<div class="month-now-year">2024</div>
 			</div>
-			<div class="month-leftright calendarMonth">></div>
+			<div class="month-leftright calendar-months">></div>
+		</div>
+		<div class="calendar-calendar">
+			<div class="calendar-calendar-day">
+				<div class="calendar-day">SUN</div>
+				<div class="calendar-day">MON</div>
+				<div class="calendar-day">TUE</div>
+				<div class="calendar-day">WED</div>
+				<div class="calendar-day">THU</div>
+				<div class="calendar-day">FRI</div>
+				<div class="calendar-day">SAT</div>
+			</div>
 		</div>
 		<div class="calendar-challenge">
 			<img class="calendar-challenge-image" src="/public/images/lucky-sad.png"></img>
@@ -28,14 +39,16 @@
 export default {
 	name: "CalendarPage",
 	data(){
-		return{}
+		return{
+			date:1
+		}
 	}
 };
 </script>
 
 <style scoped>
 .calendar-container{
-	height: 100vh;
+	height: 100%;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
@@ -44,7 +57,7 @@ export default {
 .calendar-logo{
 	margin-top: 51px;
 	height: 68px;
-	width: 100vh;
+	width: 100%;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
@@ -61,10 +74,77 @@ export default {
 	font-size: 10px;
 	/* border: 1px solid black; */
 }
+.calendar-month{
+	width:100%;
+	height: 72px;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	color:#958565;
+	/* border: 1px solid black; */
+}
+.calendar-months{
+	margin-right: 115px;
+	font-size: 32px;
+}
+.calendar-months:last-child{
+	margin-right: 0px;
+}
+.month-now{
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+.month-now-month{
+	font-size: 48px;
+	margin-bottom: 5px;
+}
+.month-now-year{
+	font-size:10px;
+	margin-bottom: 10px;
+}
+.calendar-calendar{
+	width:100%;
+	height:415px;
+	/* border:1px solid red; */
+}
+.calendar-calendar-day{
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	width:100%;
+	height: 46px;
+	/* border:1px solid blue; */
+}
+.calendar-day{
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	margin-right: 18px;
+	width:28px;
+	height:27px;
+	color: #958565;
+	font-size: 18px;
+	border-bottom:2px solid #958565;
+	/* border:1px solid green; */
+}
+.calendar-day:first-child{
+	color: #DD6262;
+	border-bottom: 2px solid #DD6262;
+}
+.calendar-day:last-child{
+	margin-right: 0px;
+	color:#737FE9;
+	border-bottom: 2px solid #737FE9;
+}
 .calendar-challenge{
 	position: absolute;
 	bottom: 94px;
-	width: 100vh;
+	width: 100%;
 	height: 141px;
 	display: flex;
 	flex-direction: row;
@@ -96,42 +176,11 @@ export default {
 	/* border: 1px solid black; */
 }
 .footer-menu-image{
-	width: 42px;
-	height: 60px;
-	margin-right:70px;
+	width: 40px;
+	height: 55px;
+	margin-right:60px;
 }
 .footer-menu-image:last-child{
 	margin-right: 0;
-}
-.calendar-month{
-	width:100vh;
-	height: 72px;
-	display: flex;
-	flex-direction: row;
-	justify-content: center;
-	align-items: center;
-	color:#958565;
-	/* border: 1px solid black; */
-}
-.calendarMonth{
-	margin-right: 115px;
-	font-size: 32px;
-}
-.calendarMonth:last-child{
-	margin-right: 0px;
-}
-.month-now{
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-}
-.month-now-month{
-	font-size: 48px;
-	margin-bottom: 5px;
-}
-.month-now-year{
-	font-size:10px;
-	margin-bottom: 10px;
 }
 </style>
