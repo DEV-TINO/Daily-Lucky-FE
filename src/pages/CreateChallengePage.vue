a38efb2bad3f9e206a2f99bc052a8714262126fc
김소진 <dfsa356@naver.com>
2024-05-11 14:08:16 +0900

    feat : create CreateChallengePage

---
+++ src/pages/CreateChallengePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="create-challenge-container">
+    <!-- Top Nav -->
+    <div></div>
+    <!-- Create Challenge -->
+    <div></div>
+    <!-- Select Date -->
+    <div></div>
+    <!-- Save Button -->
+    <div></div>
+    <!-- Bottom Nav -->
+    <div></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CreateChallengePage",
+  };
+</script>
+
+<style lang="scss" scoped></style>
