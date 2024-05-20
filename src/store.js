e159fd5f1b330e2be428696b6f5dc7700f4680d3
김소진 <dfsa356@naver.com>
2024-05-20 19:37:25 +0900

    feat : use store

---
+++ src/store.js
@@ -0,0 +1,42 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+    return {
+      calendarSelected: {
+        year: 2001,
+        month: 12,
+        date: 14,
+        day: "SUN",
+      },
+      posts: [],
+    };
+  },
+  mutations: {
+    setCalendarSelected(state, selected) {
+      state.calendarSelected = { ...selected };
+    },
+    addPost(state, post) {
+      console.log("before add post", state.posts);
+      state.posts.push(post);
+      console.log("after add post", state.posts);
+    },
+    updatePost(state, post) {
+      const index = state.posts.findIndex((currentPost) => {
+        return (
+          currentPost.year === post.year &&
+          currentPost.month === post.month &&
+          currentPost.date === post.date &&
+          currentPost.day === post.day
+        );
+      });
+      console.log("[Update Post]", index);
+      console.log("[Update Post]", state.posts);
+      if (index !== -1) {
+        state.posts[index] = post;
+      }
+    },
+  },
+});
+
+export default store;
