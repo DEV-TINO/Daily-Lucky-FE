e8b218f09191f0d1df1c3fb06a13bc694a39f77e
김소진 <dfsa356@naver.com>
2024-06-19 20:41:13 +0900

    refactor : make SelectEmoji to component

--- src/pages/WritePage.vue
+++ src/pages/WritePage.vue
@@ -14,28 +14,10 @@
         </div>
       </div>
       <!-- Select Emoji -->
-      <div class="today-moods">
-        <div class="title">
-          <div class="main-color">
-            Today's <span class="is-red">Mood!</span>
-          </div>
-        </div>
-        <div class="moods-emojis">
-          <div
-            class="emoji"
-            :class="emojiSelected(emoji)"
-            v-for="(emoji, index) in emojis"
-            :key="index"
-            @click="emojiType = emoji"
-          >
-            <img :class="emoji" :src="`/images/lucky-${emoji}.png`" />
-            <div class="text is-red">
-              {{ emoji }}
-            </div>
-          </div>
-        </div>
-      </div>
-
+      <SelectEmoji
+        :emojiSelected="emojiSelected"
+        @updateEmojiType="updateEmojiType"
+      />
       <!-- Write Contents -->
       <div class="writing-container background-color">
         <div class="date">
@@ -92,9 +74,13 @@
 </template>
 
 <script>
+  import SelectEmoji from "@/components/SelectEmoji.vue";
   import { mapState } from "vuex";
   export default {
     name: "WritePage",
+    components: {
+      SelectEmoji,
+    },
     computed: {
       ...mapState(["months", "daysFullNames", "bottomMenu", "emojis"]),
     },
@@ -124,6 +110,9 @@
       }
     },
     methods: {
+      updateEmojiType(emoji) {
+        this.emojiType = emoji;
+      },
       handleClickWriteDiary() {
         const post = {
           ...this.$store.state.calendarSelected,
@@ -186,63 +175,6 @@
       right: 0px;
       height: 35px;
       width: 80px;
-    }
-    .today-moods {
-      width: 100%;
-      .title {
-        height: 40px;
-        display: flex;
-        flex-direction: row;
-        align-items: flex-end;
-        padding-left: 29px;
-        font-size: 24px;
-      }
-      .moods-emojis {
-        width: 100%;
-        height: 85px;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: center;
-        .emoji {
-          width: 90px;
-          height: 84px;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-
-          .lucky {
-            margin-top: 30px;
-            width: 59px;
-            height: 55px;
-          }
-          .happy {
-            margin-top: 20px;
-            width: 75px;
-            height: 62px;
-          }
-          .sad {
-            margin-top: 22px;
-            width: 68px;
-            height: 56px;
-          }
-          .angry {
-            margin-top: 8px;
-            width: 75px;
-            height: 70px;
-          }
-        }
-      }
-      .text {
-        width: 100%;
-        height: 15px;
-        font-size: 10px;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        gap: 72px;
-      }
     }
     .writing-container {
       width: 100%;
