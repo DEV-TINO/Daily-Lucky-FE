0724d5a0b6256d095a3181866f5e65ef702c72ec
김소진 <dfsa356@naver.com>
2024-06-22 22:01:05 +0900

    refactor : make CreateChallenge to component

--- src/pages/CreateChallengePage.vue
+++ src/pages/CreateChallengePage.vue
@@ -4,23 +4,8 @@
 
     <div class="create-challenge-box">
       <!-- Create Challenge -->
-      <div class="container">
-        <div class="header main-color">Create Challenge!</div>
-        <div class="contents">
-          <div class="title main-color">Title</div>
-          <textarea
-            v-model="challengeTitle"
-            class="title-textarea main-color"
-            placeholder="Write challenge title"
-          ></textarea>
-          <div class="content main-color">Contents</div>
-          <textarea
-            v-model="challengeContents"
-            class="content-textarea main-color"
-            placeholder="Write challenge contents"
-          ></textarea>
-        </div>
-      </div>
+      <CreateChallenge v-model="challenge" />
+
       <!-- Select Date -->
       <div class="select-date-container">
         <div class="header main-color">Due to</div>
@@ -71,16 +56,22 @@
 </template>
 
 <script>
+  import CreateChallenge from "@/components/CreateChallenge.vue";
   import { mapState } from "vuex";
   export default {
     name: "CreateChallengePage",
+    components: {
+      CreateChallenge,
+    },
     computed: {
       ...mapState(["months", "days"]),
     },
     data() {
       return {
-        challengeTitle: "",
-        challengeContents: "",
+        challenge: {
+          title: "",
+          contents: "",
+        },
         startDate: "",
         endDate: "",
         startDateOptions: [],
@@ -137,8 +128,8 @@
 
       handleClickSaveChallenge() {
         const newChallenge = {
-          title: this.challengeTitle,
-          content: this.challengeContents,
+          title: this.challenge.title,
+          content: this.challenge.contents,
           startDate: this.startDate,
           dueDate: this.endDate,
         };
@@ -164,75 +155,6 @@
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    .container {
-      height: auto;
-      width: 100%;
-      margin-top: 29px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      .header {
-        height: 30px;
-        width: 333px;
-        display: flex;
-        justify-content: flex-start;
-        font-size: 24px;
-      }
-      .contents {
-        height: 220px;
-        width: 80%;
-        margin-top: 17px;
-        font-size: 24px;
-        .title-textarea {
-          width: 100%;
-          height: 37px;
-          margin-bottom: 15px;
-          box-sizing: border-box;
-          background-image: url("/images/line.png");
-          background-position: bottom;
-          background-size: contain;
-          background-repeat: no-repeat;
-          font-size: 14px;
-          text-align: start;
-          line-height: 35px;
-          font-family: "custom-font";
-          background-color: transparent;
-          border: none;
-          overflow: clip;
-          resize: none;
-          text-wrap: nowrap;
-        }
-        .title-textarea::placeholder {
-          color: #ccc6ba;
-        }
-        .title-textarea:focus {
-          outline: none;
-        }
-        .content-textarea {
-          width: 100%;
-          height: 80px;
-          box-sizing: border-box;
-          background-image: url("/images/line2.png");
-          background-position: bottom;
-          background-size: contain;
-          background-repeat: no-repeat;
-          font-size: 14px;
-          text-align: start;
-          line-height: 40px;
-          font-family: "custom-font";
-          background-color: transparent;
-          border: none;
-          overflow: clip;
-          resize: none;
-        }
-        .content-textarea::placeholder {
-          color: #ccc6ba;
-        }
-        .content-textarea:focus {
-          outline: none;
-        }
-      }
-    }
     .select-date-container {
       width: 80%;
       height: 293px;
